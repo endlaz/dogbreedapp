9ff3dbeff954c070d578088f4de89df9472fa3ac
endlaz <54072649+endlaz@users.noreply.github.com>
2019-09-17 15:25:33 +0200

    Revert "Move dog API call to app.js"

--- src/components/DogsListContainer.js
+++ src/components/DogsListContainer.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
+import request from 'superagent'
 import DogsList from './DogsList'
 import { connect } from 'react-redux'
 
 class DogsListContainer extends Component {
+ 
+
+  componentDidMount() {
+    request
+      .get('https://dog.ceo/api/breeds/list/all')
+      .then(response => {
+        this.props.dispatch({
+          type: 'SET_DOGSLIST',
+          payload: Object.keys(response.body.message)
+        })
+      })
+      .catch(console.error)
+  }
+
   render() {
     return <DogsList dogBreeds={this.props.dogsList} />
   }
