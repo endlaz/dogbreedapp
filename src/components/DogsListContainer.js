4c92edb3b859e217aceb9763d4e67e33dc11438f
endlaz <lazlovdende@hotmail.com>
2019-09-17 12:00:50 +0200

    Move dog API call to app.js

--- src/components/DogsListContainer.js
+++ src/components/DogsListContainer.js
@@ -1,23 +1,8 @@
 import React, { Component } from 'react'
-import request from 'superagent'
 import DogsList from './DogsList'
 import { connect } from 'react-redux'
 
 class DogsListContainer extends Component {
- 
-
-  componentDidMount() {
-    request
-      .get('https://dog.ceo/api/breeds/list/all')
-      .then(response => {
-        this.props.dispatch({
-          type: 'SET_DOGSLIST',
-          payload: Object.keys(response.body.message)
-        })
-      })
-      .catch(console.error)
-  }
-
   render() {
     return <DogsList dogBreeds={this.props.dogsList} />
   }
