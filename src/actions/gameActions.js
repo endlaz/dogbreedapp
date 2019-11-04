62168e6ebe103b50bf3ed57a2bec4adcb1c575bd
endlaz <lazlovdende@hotmail.com>
2019-11-04 14:52:47 +0100

    Reset difficulty in gameReducer.js on reset score action

--- src/actions/gameActions.js
+++ src/actions/gameActions.js
@@ -10,80 +10,80 @@ export const START_GAME = 'START_GAME'
 export const STOP_GAME = 'STOP_GAME'
 
 export function startGame() {
-    return ({
-        type: START_GAME,
-        payload: true
-    })
+  return ({
+    type: START_GAME,
+    payload: true
+  })
 }
 
 export function stopGame() {
-    return ({
-        type: STOP_GAME,
-        payload: false
-    })
+  return ({
+    type: STOP_GAME,
+    payload: false
+  })
 }
 
 export function setInitialGameBreeds(dogsList, level) {
-    const randomBreeds = _.sampleSize(dogsList, (level * 3))
-    return (dispatch, getState) => {
-        if (getState().gameReducer.breeds.length > 0) return
+  const randomBreeds = _.sampleSize(dogsList, (level * 3))
+  return (dispatch, getState) => {
+    if (getState().gameReducer.breeds.length > 0) return
 
-        dispatch({
-            type: SET_INITIAL_BREEDS,
-            payload: randomBreeds
-        })
-    }
+    dispatch({
+      type: SET_INITIAL_BREEDS,
+      payload: randomBreeds
+    })
+  }
 }
 
 export const setQuestion = () => {
-    return (dispatch, getState) => {
-        const randomDog = _.sample(getState().gameReducer.breeds);
-        const wrongDogs = _.sampleSize(
-            getState().gameReducer.breeds.filter(
-                function (e) { return this.indexOf(e) < 0 },
-                randomDog
-            ), 2)
-        console.log('Correct dog:', randomDog);
-        
-        // RESET HINT
+  return (dispatch, getState) => {
+    const randomDog = _.sample(getState().gameReducer.breeds);
+    const wrongDogs = _.sampleSize(
+      getState().gameReducer.breeds.filter(
+        function (e) { return this.indexOf(e) < 0 },
+        randomDog
+      ), 2)
+    console.log('Correct dog:', randomDog);
+
+    // RESET HINT
+    dispatch({
+      type: RESET_HINT
+    })
+    // Fetch random image for correct dog
+    request.get(`https://dog.ceo/api/breed/${randomDog}/images/random`)
+      .then(response => {
+        // If we have an image, save breed name and image as correct answer
         dispatch({
-            type: RESET_HINT
+          type: SET_CORRECT_ANSWER,
+          payload: { breed: randomDog, image: response.body.message }
         })
-        // Fetch random image for correct dog
-        request.get(`https://dog.ceo/api/breed/${randomDog}/images/random`)
-            .then(response => {
-                // If we have an image, save breed name and image as correct answer
-                dispatch({
-                    type: SET_CORRECT_ANSWER,
-                    payload: { breed: randomDog, image: response.body.message }
-                })
-            })
+      })
 
-        let wrongImages = []
-        new Promise((resolve, reject) => {
-            resolve(request.get(`https://dog.ceo/api/breed/${wrongDogs[0]}/images/random`))
+    let wrongImages = []
+    new Promise((resolve, reject) => {
+      resolve(request.get(`https://dog.ceo/api/breed/${wrongDogs[0]}/images/random`))
+    })
+      .then((result) => {
+        wrongImages.push(result.body.message);
+        return request.get(`https://dog.ceo/api/breed/${wrongDogs[1]}/images/random`)
+      })
+      .then((result) => {
+        wrongImages.push(result.body.message)
+
+        dispatch({
+          type: SET_WRONG_ANSWERS,
+          payload: [
+            { breed: wrongDogs[0], image: wrongImages[0] },
+            { breed: wrongDogs[1], image: wrongImages[1] }
+          ]
         })
-            .then((result) => {
-                wrongImages.push(result.body.message);
-                return request.get(`https://dog.ceo/api/breed/${wrongDogs[1]}/images/random`)
-            })
-            .then((result) => {
-                wrongImages.push(result.body.message)
-
-                dispatch({
-                    type: SET_WRONG_ANSWERS,
-                    payload: [
-                        {breed: wrongDogs[0], image: wrongImages[0]},
-                        {breed: wrongDogs[1], image: wrongImages[1]}
-                    ]
-                })
-            });
-    }
+      });
+  }
 }
 
 export const showBreedHint = (value) => {
-    return ({
-        type: SHOW_BREED_HINT,
-        payload: value
-    })
+  return ({
+    type: SHOW_BREED_HINT,
+    payload: value
+  })
 }
