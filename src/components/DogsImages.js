1bb79fce8c96846150a9defad663c08d1fe8d2a7
endlaz <lazlovdende@hotmail.com>
2019-11-04 14:55:08 +0100

    Fix go back link on dog images page

--- src/components/DogsImages.js
+++ src/components/DogsImages.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom"
 import PropTypes from 'prop-types'
 
 export default function DogsImages(props) {
-  const {images} = props
+  const { images } = props
   return (
     <div>
       <h1>Images for {props.breed}</h1>
       <div>
-        {images? images.map(url => <img key={url} src={url} alt="images" />): "Loading..."}
+        {images ? images.map(url => <img key={url} src={url} alt="images" />) : "Loading..."}
       </div>
-      <Link to="/">Go back</Link>
+      <Link to="/dogslist">Go back</Link>
     </div>
   )
 }
